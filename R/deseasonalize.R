a656492eae0891f243a2952e8bba2f7b573f9a9d
Jose Ignacio Garza Santos <jose_garza@hacienda.gob.mx>
2024-04-19 14:27:13 -0600

    changed the definition of deseasonalize to make it more versatile.

--- R/deseasonalize.R
+++ R/deseasonalize.R
@@ -3,16 +3,14 @@
 #' @param data A column with numerical data
 #' @param .start Argument passed to seas function
 #' @param .frequency Argument passed to seas function
+#' @param ... Arguments passed to seas from seasonal
 #'
 #' @importFrom seasonal seas
 #' @export
-deseasonalize <- function(data, .start = c(2003, 1), .frequency = 12) {
+deseasonalize <- function(data, .start = c(2003, 1), .frequency = 12, ...) {
   data_sa <- data |>
-    base::log() |>
     stats::ts(start = .start, frequency = .frequency) |>
-    seasonal::seas() |>
-    stats::predict() |>
-    base::exp()
-
+    seasonal::seas(...) |>
+    stats::predict()
   return(data_sa)
 }
